233ef24d3432c691862a9d8a5bf910169b4cd11c
bwgcarey92 <148415061+bwgcarey92@users.noreply.github.com>
2024-07-03 22:16:37 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
-import { resolve } from "path"
-import { mainModule } from "process"
 import {defineConfig} from "vite"
 
 export default defineConfig({
-	build: {
-		rollupOptions : {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				nested: resolve(__dirname, "nested/index.html")
-			}
-		}
-	},
 	plugins: [
 		
 	]
